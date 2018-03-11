6c1fa224ba555aec6e60b3c3fb8a79523a6f2a42
Dominik Palacz <palacz.dominik@gmail.com>
2018-03-11 10:03:29 +0100

    added function to game

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let characters = ['clubs ♣', 'diamonds ♦', 'hearts ♥', 'spades ♠'];
 let cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-let deck = [];
 
-for (i=0; i<characters.length; i++) {
-  for (j=0; j<cards.length; j++) {
-    deck.push(characters[i] + ' of ' + cards[j] );
+function createDeck() {
+  let deck = [];
+  for (i=0; i<characters.length; i++) {
+    for (j=0; j<cards.length; j++) {
+      deck.push(characters[i] + ' of ' + cards[j] );
+    };
   };
+  return deck;
 };
 
+function getNextCard() {
+  return deck.shift()
+};
+
+let deck = createDeck();
+
+let playerCards = [getNextCard(), getNextCard()];
+
 console.log('Welcome to Blackjack!');
-console.log(deck);
+console.log('You are dealt: ');
+console.log(playerCards[0]);
+console.log(playerCards[1]);
